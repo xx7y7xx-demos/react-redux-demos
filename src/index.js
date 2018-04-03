d84cfa384ff8bf10d7f8767b00f02cc2fe570d71
Devin Chen <d3vin.chen@gmail.com>
2018-04-04 02:17:06 +0800

    Add redux demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers';
+import App from './App';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
